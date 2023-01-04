1ae8cb328bfd73a25b30f8eec3c3b7c9d8487446
cjones1047 <cjmj1047@gmail.com>
2023-01-04 12:48:55 -0600

    .gitignore and .env created in root directory; Sheety auth token and endpoint for Google Sheet containing flight deals added to .env; DataManager instantiation gets all data from flight deals Google Sheet

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,16 @@
+import requests
+import os
+import dotenv
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    # This class is responsible for talking to the Google Sheet.
+    def __init__(self):
+        dotenv.load_dotenv()
+        self.sheety_flight_deals_endpoint = os.getenv("SHEETY_FLIGHT_DEALS_ENDPOINT")
+        self.sheety_flight_deals_token = os.getenv("SHEETY_FLIGHT_DEALS_TOKEN")
+        self.sheety_headers = {
+            "Authorization": f"Bearer {self.sheety_flight_deals_token}"
+        }
+        sheety_json = requests.get(url=self.sheety_flight_deals_endpoint, headers=self.sheety_headers).json()
+        print(sheety_json)
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to
+# achieve the program requirements.
+
+from data_manager import DataManager
+
+data_manager = DataManager()
